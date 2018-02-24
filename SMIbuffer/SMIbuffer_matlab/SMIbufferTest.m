20b565be88b8de463952a601ff480af8b6967d08
Diederick C. Niehorster <dcnieho@gmail.com>
2018-02-24 23:38:09 +0100

    fixed up testing scripts a bit

--- SMIbuffer/SMIbuffer_matlab/SMIbufferTest.m
+++ SMIbuffer/SMIbuffer_matlab/SMIbufferTest.m
@@ -2,22 +2,9 @@
 
 sampEvtBuffers = SMIbuffer();
 
+success = sampEvtBuffers.startSampleBuffering()
 
+WaitSecs(4);
+samples = sampEvtBuffers.getSamples();
 
-dataMsgs2 = sampEvtBuffers.getData;
-
-
-% send a bunch of commands
-for i=1:10
-    sampEvtBuffers.send('cmd');  % testing empty string in message struct
-end
-cmdMsgs = sampEvtBuffers.getCommands;
-
-% send exit msg
-fprintf('nThreads active: %i\n',sampEvtBuffers.checkReceiverThreads);
-sampEvtBuffers.send('exit');
-fprintf('nThreads active: %i\n',sampEvtBuffers.checkReceiverThreads);
-
-% clean up
-sampEvtBuffers.deInit();
-fprintf('nThreads active: %i\n',sampEvtBuffers.checkReceiverThreads);
+sampEvtBuffers.stopSampleBuffering(true);   % optional input indicating whether to also destroy buffer (delete samples) or not
--- SMIbuffer/SMIbuffer_python/test.py
+++ SMIbuffer/SMIbuffer_python/test.py
@@ -1,5 +1,12 @@
 ﻿import SMIbuffer
+import time
 
 sampEvtBuffers = SMIbuffer.SMIbuffer()
 
 success = sampEvtBuffers.startSampleBuffering()
+print success
+
+time.sleep(5)
+samples = sampEvtBuffers.getSamples()
+
+sampEvtBuffers.stopSampleBuffering(true)    # optional input indicating whether to also destroy buffer (delete samples) or not
