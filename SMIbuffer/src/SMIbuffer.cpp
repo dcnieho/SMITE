0c89ea164fae148b64f6bbc7f11ec4f499d2c212
Diederick C. Niehorster <dcnieho@gmail.com>
2018-07-22 00:05:02 +0200

    SMIbuffer.cpp: problems with no data was because classPtr was zerod by destructor. Matlab seems to call destructor on a class instance after another instance overwriting it is constructed. So that zeroed out by beautiful and sadly required global variable... so now only set classPtr (which was renamed for clarity) only just before it is first used, which should be way out of such problems. Guess we still have an issue if users runs multiple instances of same class from same Matlab instance, but then we don't support that anyway

--- SMIbuffer/src/SMIbuffer.cpp
+++ SMIbuffer/src/SMIbuffer.cpp
@@ -1,7 +1,7 @@
 #include "SMIbuffer/SMIbuffer.h"
 
 namespace {
-    SMIbuffer* classPtr = nullptr;
+    SMIbuffer* SMIbufferClassInstance = nullptr;  // for plain C callback to be able to call into the class
 
     template <typename T>
     inline std::vector<T> getData(mpmc_bounded_queue<T>* dataBuffer_, bool justDump_=false)
@@ -25,15 +25,15 @@ namespace {
 
 int __stdcall SMISampleCallback(SampleStruct sampleData_)
 {
-    if (classPtr && classPtr->_sampleData)
-        classPtr->_sampleData->enqueue(sampleData_);
+    if (SMIbufferClassInstance && SMIbufferClassInstance->_sampleData)
+        SMIbufferClassInstance->_sampleData->enqueue(sampleData_);
     return 1;
 }
 
 int __stdcall SMIEventCallback(EventStruct eventData_)
 {
-    if (classPtr && classPtr->_eventData)
-        classPtr->_eventData->enqueue(eventData_);
+    if (SMIbufferClassInstance && SMIbufferClassInstance->_eventData)
+        SMIbufferClassInstance->_eventData->enqueue(eventData_);
     return 1;
 }
 
@@ -41,28 +41,32 @@ int __stdcall SMIEventCallback(EventStruct eventData_)
 
 
 SMIbuffer::SMIbuffer()
-{
-    classPtr = this;
-}
+{}
 
 SMIbuffer::~SMIbuffer()
 {
     stopSampleBuffering(true);
     stopEventBuffering (true);
-    classPtr = nullptr;
+    SMIbufferClassInstance = nullptr;
 }
 
 int SMIbuffer::startSampleBuffering(size_t bufferSize_ /*= 1<<22*/)
 {
+    SMIbufferClassInstance = this;    // make sure its set. doing this in constructor is dangerous, as destructor of an older instance of class (from precious matlab script execution) may be lingering, and destroyed **after** this instance is created. So update SMIbufferClassInstance as late as possible
+
     if (!_sampleData)
         _sampleData = new mpmc_bounded_queue<SampleStruct>(bufferSize_);
+
     return iV_SetSampleCallback(SMISampleCallback);
 }
 
 int SMIbuffer::startEventBuffering(size_t bufferSize_ /*= 1<<20*/)
 {
+    SMIbufferClassInstance = this;
+
     if (!_eventData)
         _eventData = new mpmc_bounded_queue<EventStruct>(bufferSize_);
+    
     return iV_SetEventCallback(SMIEventCallback);
 }
 
