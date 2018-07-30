648958b1077d201c9b04e7d07dd7316412f57b84
Diederick C. Niehorster <dcnieho@gmail.com>
2018-07-30 21:12:48 +0200

    SMIbuffer: realized this implementation was too simple. Per instance in the instance list, we need to also keep track of what type of data (sample and/or event) it is registered for. Implemented, but untested

--- SMIbuffer/src/SMIbuffer.cpp
+++ SMIbuffer/src/SMIbuffer.cpp
@@ -1,6 +1,8 @@
 #include "SMIbuffer/SMIbuffer.h"
 #include <shared_mutex>
 #include <vector>
+#include <algorithm>
+#include <utility>
 
 namespace {
     typedef std::shared_timed_mutex mutex_type;
@@ -10,7 +12,8 @@ namespace {
     read_lock  lockForReading() { return  read_lock(m); }
     write_lock lockForWriting() { return write_lock(m); }
 
-    std::vector<SMIbuffer*> SMIbufferClassInstances;  // for plain C callback to be able to call into the class instances
+    typedef std::pair<SMIbuffer*, unsigned int> instance_type;
+    std::vector<instance_type> SMIbufferClassInstances;  // for plain C callback to be able to call into the class instances
 
     template <typename T>
     inline std::vector<T> getData(mpmc_bounded_queue<T>* dataBuffer_, bool justDump_=false)
@@ -32,25 +35,25 @@ namespace {
     }
 }
 
-int __stdcall SMISampleCallback(SampleStruct sampleData_)
+int __stdcall SMISampleCallback(SampleStruct sample_)
 {
     auto l = lockForReading();
     for each (auto&& instance in SMIbufferClassInstances)
     {
-        if (instance->_sampleData)
-            instance->_sampleData->enqueue(sampleData_);
+        if ((instance.second & 1) && instance.first->_sampleData)
+            instance.first->_sampleData->enqueue(sample_);
     }
 
     return 1;
 }
 
-int __stdcall SMIEventCallback(EventStruct eventData_)
+int __stdcall SMIEventCallback(EventStruct event_)
 {
     auto l = lockForReading();
     for each (auto&& instance in SMIbufferClassInstances)
     {
-        if (instance->_eventData)
-            instance->_eventData->enqueue(eventData_);
+        if ((instance.second & 2) && instance.first->_eventData)
+            instance.first->_eventData->enqueue(event_);
     }
 
     return 1;
@@ -60,24 +63,26 @@ int __stdcall SMIEventCallback(EventStruct eventData_)
 
 
 SMIbuffer::SMIbuffer()
-{
-    auto l = lockForWriting();
-    SMIbufferClassInstances.push_back(this);
-}
+{}
 
 SMIbuffer::~SMIbuffer()
 {
     stopSampleBuffering(true);
     stopEventBuffering (true);
-
-    auto l = lockForWriting();
-    SMIbufferClassInstances.erase(std::remove(SMIbufferClassInstances.begin(), SMIbufferClassInstances.end(), this), SMIbufferClassInstances.end());
 }
 
 int SMIbuffer::startSampleBuffering(size_t bufferSize_ /*= 1<<22*/)
 {
     if (!_sampleData)
         _sampleData = new mpmc_bounded_queue<SampleStruct>(bufferSize_);
+
+    // if instance already in set, register it for samples, else add to set
+    auto l = lockForWriting();
+    auto it = std::find_if(SMIbufferClassInstances.begin(), SMIbufferClassInstances.end(), [this](const instance_type& a_) {return a_.first == this;});
+    if (it == SMIbufferClassInstances.end())
+        SMIbufferClassInstances.emplace_back(this, 1);
+    else
+        it->second |= 1;
 
     return iV_SetSampleCallback(SMISampleCallback);
 }
@@ -86,6 +91,14 @@ int SMIbuffer::startEventBuffering(size_t bufferSize_ /*= 1<<20*/)
 {
     if (!_eventData)
         _eventData = new mpmc_bounded_queue<EventStruct>(bufferSize_);
+
+    // if instance already in set, register it for events, else add to set
+    auto l = lockForWriting();
+    auto it = std::find_if(SMIbufferClassInstances.begin(), SMIbufferClassInstances.end(), [this](const instance_type& a_) {return a_.first == this; });
+    if (it == SMIbufferClassInstances.end())
+        SMIbufferClassInstances.emplace_back(this, 2);
+    else
+        it->second |= 2;
 
     return iV_SetEventCallback(SMIEventCallback);
 }
@@ -102,22 +115,48 @@ void SMIbuffer::clearEventBuffer()
 
 void SMIbuffer::stopSampleBuffering(bool deleteBuffer_)
 {
-    iV_SetSampleCallback(nullptr);
     if (_sampleData && deleteBuffer_)
     {
         delete _sampleData;
         _sampleData = nullptr;
     }
+
+    // remove registration for event buffering for this instance
+    auto l = lockForWriting();
+    auto it = std::find_if(SMIbufferClassInstances.begin(), SMIbufferClassInstances.end(), [this](const instance_type& a_) {return a_.first == this; });
+    if (it != SMIbufferClassInstances.end())
+        it->second &= ~(1);
+
+    // if no callback registrations left, remove instance from instance list
+    if (!it->second)
+        SMIbufferClassInstances.erase(it);
+
+    // if no instances left, remove callback function
+    if (SMIbufferClassInstances.empty())
+    iV_SetSampleCallback(nullptr);
 }
 
 void SMIbuffer::stopEventBuffering(bool deleteBuffer_)
 {
-    iV_SetEventCallback(nullptr);
     if (_eventData && deleteBuffer_)
     {
         delete _eventData;
         _eventData = nullptr;
     }
+
+    // remove registration for event buffering for this instance
+    auto l = lockForWriting();
+    auto it = std::find_if(SMIbufferClassInstances.begin(), SMIbufferClassInstances.end(), [this](const instance_type& a_) {return a_.first == this; });
+    if (it != SMIbufferClassInstances.end())
+        it->second &= ~(2);
+
+    // if no callback registrations left, remove instance from instance list
+    if (!it->second)
+        SMIbufferClassInstances.erase(it);
+
+    // if no instances left, remove callback function
+    if (SMIbufferClassInstances.empty())
+        iV_SetEventCallback(nullptr);
 }
 
 std::vector<SampleStruct> SMIbuffer::getSamples()
