2c78a079ec04c6455b1c177e9f5b898ae3eeb5b0
Diederick C. Niehorster <dcnieho@gmail.com>
2018-10-15 23:25:59 +0200

    SMIbuffer: core lib now has vector as underlying storage type (so we can grow till memory runs out) and now has both consume and peek from the sample/event buffer

--- SMIbuffer/SMIbuffer/SMIbuffer.h
+++ SMIbuffer/SMIbuffer/SMIbuffer.h
@@ -6,7 +6,19 @@
 #else
 #	pragma comment(lib, "iViewXAPI.lib")
 #endif
-#include "mpmcBoundedQueue.h"
+
+
+namespace SMIbuff
+{
+    // default argument values
+    constexpr size_t g_sampleBufDefaultSize = 1 << 22;
+
+    constexpr size_t g_eventBufDefaultSize = 1 << 14;
+
+    constexpr bool   g_stopBufferEmptiesDefault = false;
+    constexpr size_t g_consumeDefaultAmount = -1;
+    constexpr size_t g_peekDefaultAmount = 1;
+}
 
 
 
@@ -18,26 +30,40 @@ public:
 
     void setEyeSwap(const bool& needsEyeSwap_);
 
-    int startSampleBuffering(size_t bufferSize_ = 1<<22);
-    int startEventBuffering (size_t bufferSize_ = 1<<20);
+    int startSampleBuffering(size_t initialBufferSize_ = SMIbuff::g_sampleBufDefaultSize);
+    int startEventBuffering (size_t initialBufferSize_ = SMIbuff::g_eventBufDefaultSize);
     // clear all buffer contents
     void clearSampleBuffer();
     void clearEventBuffer ();
     // stop optionally deletes the buffer
-    void stopSampleBuffering(bool deleteBuffer_);
-    void stopEventBuffering (bool deleteBuffer_);
+    void stopSampleBuffering(bool emptyBuffer_ = SMIbuff::g_stopBufferEmptiesDefault);
+    void stopEventBuffering (bool emptyBuffer_ = SMIbuff::g_stopBufferEmptiesDefault);
 
-    // get the data received since the last call to this function
-    std::vector<SampleStruct> getSamples();
-    std::vector<EventStruct>  getEvents ();
+    // consume samples (by default all)
+    std::vector<SampleStruct> consumeSamples(size_t firstN_ = SMIbuff::g_consumeDefaultAmount);
+    // peek samples (by default only last one, can specify how many from end to peek)
+    std::vector<SampleStruct> peekSamples(size_t lastN_ = SMIbuff::g_peekDefaultAmount);
+    // consume events (by default all)
+    std::vector<EventStruct>  consumeEvents(size_t firstN_ = SMIbuff::g_consumeDefaultAmount);
+    // peek events (by default only last one, can specify how many from end to peek)
+    std::vector<EventStruct>  peekEvents(size_t lastN_ = SMIbuff::g_peekDefaultAmount);
 
 private:
     // SMI callbacks needs to be friends
     friend int __stdcall SMISampleCallback(SampleStruct sampleData_);
     friend int __stdcall SMIEventCallback (EventStruct   eventData_);
 
+    //// generic functions for internal use
+    // helpers
+    template <typename T>  std::vector<T>&  getBuffer();
+    // generic implementations
+    template <typename T>  void             clearBuffer();
+    template <typename T>  void             stopBufferingGenericPart(bool emptyBuffer_);
+    template <typename T>  std::vector<T>   peek(size_t lastN_);
+    template <typename T>  std::vector<T>   consume(size_t firstN_);
+
 private:
-    mpmc_bounded_queue<SampleStruct>* _sampleData = nullptr;
-    mpmc_bounded_queue<EventStruct>*  _eventData  = nullptr;
-    bool                              _doEyeSwap;
+    std::vector<SampleStruct> _sampleData;
+    std::vector<EventStruct>  _eventData;
+    bool                      _doEyeSwap;
 };
--- SMIbuffer/src/SMIbuffer.cpp
+++ SMIbuffer/src/SMIbuffer.cpp
@@ -1,35 +1,41 @@
 #include "SMIbuffer/SMIbuffer.h"
+#include <vector>
+#include <shared_mutex>
+#include <algorithm>
 
 namespace {
     SMIbuffer* SMIbufferClassInstance=nullptr;  // for plain C callback to be able to call into the class instances
 
-    template <typename T>
-    inline std::vector<T> getData(mpmc_bounded_queue<T>* dataBuffer_, bool justDump_=false)
-    {
-        std::vector<T> data;
-        if (!dataBuffer_)
-            return data;
+    typedef std::shared_timed_mutex mutex_type;
+    typedef std::shared_lock<mutex_type> read_lock;
+    typedef std::unique_lock<mutex_type> write_lock;
 
-        while (true)
-        {
-            T temp;
-            bool success = dataBuffer_->dequeue(temp);
-            if (success && !justDump_)
-                data.push_back(std::move(temp));
-            else
-                break;
-        }
-        return data;
+    mutex_type g_mSamp, g_mEvent;
+
+    template <typename T>
+    read_lock  lockForReading() { return  read_lock(getMutex<T>()); }
+    template <typename T>
+    write_lock lockForWriting() { return write_lock(getMutex<T>()); }
+
+    template <typename T>
+    mutex_type& getMutex()
+    {
+        if constexpr (std::is_same<T, SampleStruct>::value)
+            return g_mSamp;
+        if constexpr (std::is_same<T, EventStruct>::value)
+            return g_mEvent;
     }
 }
 
 int __stdcall SMISampleCallback(SampleStruct sample_)
 {
-    if (SMIbufferClassInstance && SMIbufferClassInstance->_sampleData)
+    if (SMIbufferClassInstance)
     {
         if (SMIbufferClassInstance->_doEyeSwap)
             std::swap(sample_.leftEye, sample_.rightEye);
-        SMIbufferClassInstance->_sampleData->enqueue(sample_);
+
+        auto l = lockForWriting<SampleStruct>();
+        SMIbufferClassInstance->_sampleData.push_back(sample_);
     }
 
     return 1;
@@ -37,12 +43,65 @@ int __stdcall SMISampleCallback(SampleStruct sample_)
 
 int __stdcall SMIEventCallback(EventStruct event_)
 {
-    if (SMIbufferClassInstance && SMIbufferClassInstance->_eventData)
-        SMIbufferClassInstance->_eventData->enqueue(event_);
+    if (SMIbufferClassInstance)
+    {
+        auto l = lockForWriting<EventStruct>();
+        SMIbufferClassInstance->_eventData.push_back(event_);
+    }
 
     return 1;
 }
 
+
+
+
+
+// helpers to make below generic
+template <typename T>
+std::vector<T>& SMIbuffer::getBuffer()
+{
+    if constexpr (std::is_same_v<T, SampleStruct>)
+        return _sampleData;
+    if constexpr (std::is_same_v<T, EventStruct>)
+        return _eventData;
+}
+template <typename T>
+void SMIbuffer::clearBuffer()
+{
+    auto l = lockForWriting<T>();
+    getBuffer<T>().clear();
+}
+template <typename T>
+void SMIbuffer::stopBufferingGenericPart(bool emptyBuffer_)
+{
+    if (emptyBuffer_)
+        clearBuffer<T>();
+}
+template <typename T>
+std::vector<T> SMIbuffer::peek(size_t lastN_)
+{
+    auto l = lockForReading<T>();
+    auto& buf = getBuffer<T>();
+    // copy last N or whole vector if less than N elements available
+    return std::vector<T>(buf.end() - std::min(buf.size(), lastN_), buf.end());
+}
+template <typename T>
+std::vector<T> SMIbuffer::consume(size_t firstN_)
+{
+    auto l = lockForWriting<T>();
+    auto& buf = getBuffer<T>();
+
+    if (firstN_ == -1 || firstN_ >= buf.size())		// firstN_=-1 overflows, so first check strictly not needed. Better keep code legible tho
+        return std::vector<T>(std::move(buf));
+    else
+    {
+        std::vector<T> out;
+        out.reserve(firstN_);
+        out.insert(out.end(), std::make_move_iterator(buf.begin()), std::make_move_iterator(buf.begin() + firstN_));
+        buf.erase(buf.begin(), buf.begin() + firstN_);
+        return out;
+    }
+}
 
 
 
@@ -61,68 +120,65 @@ void SMIbuffer::setEyeSwap(const bool& needsEyeSwap_)
     _doEyeSwap = needsEyeSwap_;
 }
 
-int SMIbuffer::startSampleBuffering(size_t bufferSize_ /*= 1<<22*/)
+int SMIbuffer::startSampleBuffering(size_t initialBufferSize_ /*= SMIbuff::g_sampleBufDefaultSize*/)
 {
-    if (!_sampleData)
-        _sampleData = new mpmc_bounded_queue<SampleStruct>(bufferSize_);
-
     // make sure we know what class instance should receive the data
     SMIbufferClassInstance = this;
+
+    auto l = lockForWriting<SampleStruct>();
+    _sampleData.reserve(initialBufferSize_);
 
     return iV_SetSampleCallback(SMISampleCallback);
 }
 
-int SMIbuffer::startEventBuffering(size_t bufferSize_ /*= 1<<20*/)
+int SMIbuffer::startEventBuffering(size_t initialBufferSize_ /*= SMIbuff::g_eventBufDefaultSize*/)
 {
-    if (!_eventData)
-        _eventData = new mpmc_bounded_queue<EventStruct>(bufferSize_);
-
     // make sure we know what class instance should receive the data
     SMIbufferClassInstance = this;
+
+    auto l = lockForWriting<EventStruct>();
+    _eventData.reserve(initialBufferSize_);
 
     return iV_SetEventCallback(SMIEventCallback);
 }
 
 void SMIbuffer::clearSampleBuffer()
 {
-    getData(_sampleData,true);
+    clearBuffer<SampleStruct>();
 }
 
 void SMIbuffer::clearEventBuffer()
 {
-    getData(_eventData,true);
+    clearBuffer<EventStruct>();
 }
 
-void SMIbuffer::stopSampleBuffering(bool deleteBuffer_)
+void SMIbuffer::stopSampleBuffering(bool emptyBuffer_ /*= g_stopBufferEmptiesDefault*/)
 {
-    if (_sampleData && deleteBuffer_)
-    {
-        delete _sampleData;
-        _sampleData = nullptr;
-    }
-
     // remove callback function
     iV_SetSampleCallback(nullptr);
+    stopBufferingGenericPart<SampleStruct>(emptyBuffer_);
 }
 
-void SMIbuffer::stopEventBuffering(bool deleteBuffer_)
+void SMIbuffer::stopEventBuffering(bool emptyBuffer_ /*= g_stopBufferEmptiesDefault*/)
 {
-    if (_eventData && deleteBuffer_)
-    {
-        delete _eventData;
-        _eventData = nullptr;
-    }
-
     // remove callback function
     iV_SetEventCallback(nullptr);
+    stopBufferingGenericPart<EventStruct>(emptyBuffer_);
 }
 
-std::vector<SampleStruct> SMIbuffer::getSamples()
+std::vector<SampleStruct> SMIbuffer::consumeSamples(size_t firstN_/* = g_consumeDefaultAmount*/)
 {
-    return getData(_sampleData);
+    return consume<SampleStruct>(firstN_);
 }
-
-std::vector<EventStruct> SMIbuffer::getEvents()
+std::vector<SampleStruct> SMIbuffer::peekSamples(size_t lastN_/* = g_peekDefaultAmount*/)
 {
-    return getData(_eventData);
+    return peek<SampleStruct>(lastN_);
+}
+std::vector<EventStruct> SMIbuffer::consumeEvents(size_t firstN_/* = g_consumeDefaultAmount*/)
+{
+    return consume<EventStruct>(firstN_);
+}
+std::vector<EventStruct> SMIbuffer::peekEvents(size_t lastN_/* = g_peekDefaultAmount*/)
+{
+    return peek<EventStruct>(lastN_);
 }
